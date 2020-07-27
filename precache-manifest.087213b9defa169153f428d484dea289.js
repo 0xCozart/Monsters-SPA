ae5e2c9e60b1de356b0bbba8fd188b78870ebafc
alan <alanvega002@gmail.com>
2020-07-27 03:18:13 +0000

    Updates

---
+++ precache-manifest.087213b9defa169153f428d484dea289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebce00480f4505053b8bb1635f5fe69",
+    "url": "/Monsters-SPA/index.html"
+  },
+  {
+    "revision": "e1ff18bf98315847faeb",
+    "url": "/Monsters-SPA/static/css/main.754b1ae1.chunk.css"
+  },
+  {
+    "revision": "3de74922fd73a17cbe59",
+    "url": "/Monsters-SPA/static/js/2.d13b0a8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-SPA/static/js/2.d13b0a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ff18bf98315847faeb",
+    "url": "/Monsters-SPA/static/js/main.3d8f82a0.chunk.js"
+  },
+  {
+    "revision": "817ee5d497da915ee39d",
+    "url": "/Monsters-SPA/static/js/runtime-main.3cec3a11.js"
+  }
+]);
